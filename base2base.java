7fb9d7c5f8558427188aa243fb5cb5d3d5ecbb44
Radical-Aviator <radicalaviator10@gmail.com>
2025-06-20 02:10:50 +0400

    Added conversion for bases up to base-62.

--- base2base.java
+++ base2base.java
@@ -1,98 +1,65 @@
 import java.util.*; 
-class b2bv2
-{
+class base_to_base
+{ 
     static String toDecimal (int n, String x)
     {
-        double fv = 0;
-        int xn;
-        char dc;
-        if(x.indexOf('.') < 0)
+        double fV = 0;
+        long dN;
+        char dC;
+        String rS = (x.indexOf('.') < 0)? x + ".0" : x;
+        int iP = rS.indexOf('.');
+        for (int i = iP - 1; i > -1; i--)
         {
-            for (int i = x.length() - 1; i > -1; i--)
-            {
-                dc = x.charAt(i);
-                if (Character.isLetter(dc))
-                xn = dc - 55;
-                else
-                xn = dc - 48;
-                fv += xn * (int)Math.pow(n,x.length()-1-i);
-            }
+            dC = rS.charAt(i);
+            dN = (Character.isLetter(dC))? (Character.isUpperCase(dC))? dC - 55 : dC - 61 : dC - 48;
+            fV += dN * (long)Math.pow(n,iP-1-i);
         }
-        else
+        for (int i = iP + 1; i < rS.length(); i++)
         {
-            int ip = x.indexOf('.');
-            for (int i = ip - 1; i > -1; i--)
-            {
-                dc = x.charAt(i);
-                if (Character.isLetter(dc))
-                xn = dc - 55;
-                else
-                xn = dc - 48;
-                fv += xn * (int)Math.pow(n,ip-1-i);
-            }
-            for (int i = ip + 1; i < x.length(); i++)
-            {
-                dc = x.charAt(i);
-                if (Character.isLetter(dc))
-                xn = dc - 55;
-                else
-                xn = dc - 48;
-                fv += xn * Math.pow(n,ip-i);
-            }
+            dC = rS.charAt(i);
+            dN = (Character.isLetter(dC))? (Character.isUpperCase(dC))? dC - 55 : dC - 61 : dC - 48;
+            fV += dN * Math.pow(n,iP-i);
         }
-        return String.valueOf(fv);
+        return (x.indexOf('.') < 0)? String.valueOf((long)fV) : String.valueOf(fV);
     }
     static String fromDecimal (int n, String x)
     {
-        String fv = "";   
-        int pi = (int)Double.parseDouble(x);
-        int in;
-        while (pi > 0)
+        String fV = "";   
+        long pI = (long)Double.parseDouble(x), iN;
+        while (pI > 0)
         {
-            in = pi % n;
-            if (in < 10)
-            fv = Integer.toString(in) + fv;
-            else
-            fv = (char)(in + 55) + fv;
-            pi = (pi - in) / n;
+            iN = pI % n;
+            fV = (iN < 10)? Long.toString(iN) + fV : (iN < 36)? (char)(iN + 55) + fV : (char)(iN + 61) + fV;
+            pI = (pI - iN) / n;
         }
-        int dn;
+        int dN;
         if(x.indexOf('.') > -1)
         {
-            fv += ".";
-            double pd = Double.parseDouble(x) - (int)Double.parseDouble(x);
-            int dC = 0;
-            while (pd != 0 && dC < 1)
+            fV += ".";
+            double pD = Double.parseDouble(x) - (long)Double.parseDouble(x);
+            int D = 0;
+            while (pD != 0 && D < 5)
             {
-                pd *= n;
-                dn = (int)pd;
-                if (dn < 10)
-                fv += Integer.toString(dn);
-                else
-                fv += (char)(dn + 55);
-                pd -= (int)pd;
-                dC++;
+                pD *= n;
+                dN = (int)pD;
+                fV =  (dN < 10)? fV + Long.toString(dN) : (dN < 36)? fV + (char)(dN + 55) : fV + (char)(dN + 61);
+                pD -= (int)pD;
+                D++;
             }
         }
-        return fv;
+        return fV;
     }
     public static void main (String[] args)
     {
         Scanner sc = new Scanner (System.in);
-        System.out.println("[NOTE: Bases need to be natural numbers below 37.]");
+        System.out.println("[NOTE: Bases need to be natural numbers below 63.]");
         System.out.print("Enter Input Base: ");
-        int ni = sc.nextInt();
+        int nI = sc.nextInt();
         System.out.print("Enter Number: ");
         String x = sc.next();
         System.out.print("Enter Output Base: ");
-        int no = sc.nextInt();
+        int nO = sc.nextInt();
         sc.close();
-        if (ni != no)
-        if (ni > 0 && ni < 37 && no > 0 && no < 37)
-        System.out.print("Result: " + fromDecimal(no,toDecimal(ni,x)));
-        else
-        System.out.println("Invalid Output Base!");
-        else
-        System.out.println("\n Input Base and Output Base canxt be the same.");
+        System.out.println((nI > 0 && nI < 63 && nO > 0 && nO < 63)? (nI != nO)? "Result: " + fromDecimal(nO,toDecimal(nI,x)) : "\n Input Base and Output Base cannot be the same." : "Invalid Output Base!");
     }
 }
